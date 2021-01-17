7035ebf98b2508323ba12d34b02c5ac07163c42f
LightningFox <bogdanlisichenko@gmail.com>
2021-01-17 23:13:58 +0200

    1.9v2

--- etc/gunicorn.conf.py
+++ etc/gunicorn.conf.py
@@ -1,3 +1,4 @@
 """gunicorn WSGI server configuration."""
 bind = "0.0.0.0:8080"
 workers = 1
+pythonpath = "/home/box/etc/hello.py"
--- init.sh
+++ init.sh
@@ -2,5 +2,7 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 # Gunicorn
-sudo ln -sf /home/box/web/gunicorn.conf  /etc/gunicorn.d/test
-gunicron hello:function
+sudo ln -sf /home/box/web/gunicorn.conf  /etc/gunicorn.d/hello.py
+funicorn -b hello:function 0.0.0.0:8080
+#gunicorn -c hello.py hello:functions
+# Failed test #4. Invalid Content-Type header (text/plain expected) received from nginx

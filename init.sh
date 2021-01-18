2e9edbf3e35a7a649c141e892bf9d26120db0f52
LightningFox <bogdanlisichenko@gmail.com>
2021-01-18 22:32:41 +0200

    Ну пожалуйста

--- init.sh
+++ init.sh
@@ -1,7 +1,12 @@
 # fix some problem
-sudo apt update 
-sudo apt install python3.9
-sudo pip3 install --upgrade django
+sudo apt update
+sudo apt install python3.5
+sudo apt-get install -y python3.5-dev
+sudo rm /usr/bin/python3
+sudo ln -s /usr/bin/python3.5 /usr/bin/python3
+sudo pip3 install --upgrade pip
+sudo -H pip3 install --upgrade django==2.1
+sudo -H pip3 install --upgrade gunicorn
 sudo rm /etc/nginx/sites-enabled/default
 # nginx
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default

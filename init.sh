941506eb471ecbfc2cc462998bddb8c932152501
LightningFox <bogdanlisichenko@gmail.com>
2021-01-18 20:11:28 +0200

    2.1 v1

--- init.sh
+++ init.sh
@@ -3,6 +3,7 @@ sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 # Gunicorn
 sudo ln -sf /home/box/web/gunicorn.conf  /etc/gunicorn.d/hello.py
-funicorn -b hello:function 0.0.0.0:8080
+gunicorn -b 0.0.0.0:8080 hello:function 
+gunicorn -b 0.0.0.0:8000 hello:function 
 #gunicorn -c hello.py hello:functions
 # Failed test #4. Invalid Content-Type header (text/plain expected) received from nginx

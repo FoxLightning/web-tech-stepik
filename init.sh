7022fc673ac2eae6297d95d8de8681237c25858a
LightningFox <bogdanlisichenko@gmail.com>
2021-01-18 22:25:45 +0200

    Госпади помоги

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 # fix some problem
 sudo apt update 
 sudo apt install python3.9
-sudo pip install --upgrade django
+sudo pip3 install --upgrade django
 sudo rm /etc/nginx/sites-enabled/default
 # nginx
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
